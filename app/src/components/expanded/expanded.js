cd694b479b0c31f50a8781bca26022a9d7151bb3
barifrah1 <63462394+barifrah1@users.noreply.github.com>
2020-10-06 12:39:08 +0300

    reorgnaize server side in folders, changing in participants table and expanded

---
+++ app/src/components/expanded/expanded.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { useState, useEffect } from "react";
+import "./expanded.css";
+import moment from "moment";
+import smartSearch from "smart-search";
+import ExpandedDetails from "../expandeddetails/expandeddetails";
+import SadnaotDetails from "../sadnaot_details/sadnaot_details";
+
+const Expanded = (props) => {
+  return (
+    <div className="expanded_div">
+      <span className="details">
+        <ExpandedDetails cols={props.cols} row={props.row} />
+      </span>
+      <span className="sadnaot_gifts">
+        <SadnaotDetails row={props.row} />
+      </span>
+    </div>
+  );
+};
+
+export default Expanded;
--- app/src/components/participantstable/participantstable.js
+++ app/src/components/participantstable/participantstable.js
@@ -3,8 +3,14 @@ import { useState, useEffect } from "react";
 import BootstrapTable from "react-bootstrap-table-next";
 import "./bootstrap.min.css";
 import "./react-bootstrap-table2.css";
+import "./react-bootstrap-table2-paginator.css";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
 import "./participantstable.css";
 import moment from "moment";
+import Expanded from "../expanded/expanded";
+
+const { SearchBar } = Search;
 
 function ParticipantsTable() {
   const [data, setData] = useState([{ id: 1, name: "yosi", price: 10 }]);
@@ -12,42 +18,103 @@ function ParticipantsTable() {
     {
       dataField: "id",
       text: "#",
-      sortable: true,
+      type: "number",
+      sort: true,
+      style: { "white-space": "nowrap" },
     },
 
     {
       dataField: "name",
       text: "שם",
+      type: "string",
+      sort: true,
       formatter: (cell, row) => row.Fname + " " + row.Lname,
+      style: { "white-space": "nowrap" },
     },
     {
       dataField: "category",
       text: "קטגוריה",
+      type: "string",
+      sort: true,
+      style: { "white-space": "nowrap" },
     },
     {
       dataField: "phone",
       text: "טלפון",
+      type: "string",
+      sort: true,
+      style: { "white-space": "nowrap" },
     },
     {
       dataField: "mail",
       text: "מייל",
+      type: "string",
+      sort: true,
+      style: { "white-space": "nowrap" },
     },
     {
       dataField: "sug",
       text: "סוג כרטיס",
+      type: "number",
+      sort: true,
+      style: { "white-space": "nowrap" },
     },
     {
       dataField: "reg_date",
       text: "תאריך הרשמה",
+      type: "date",
+      sort: true,
       formatter: (cell, row) => moment(cell).format("DD/MM/yyyy"),
+      style: { "white-space": "nowrap" },
     },
     {
       dataField: "payment",
       text: "תשלום",
+      type: "bool",
+      sort: true,
+      style: { "white-space": "nowrap" },
+    },
+    {
+      dataField: "city",
+      text: "יישוב",
+      type: "string",
+      sort: true,
+      hidden: true,
+      style: { "white-space": "nowrap" },
+    },
+    {
+      dataField: "vegan",
+      text: "צמחוני",
+      type: "string",
+      sort: true,
+      hidden: true,
+      style: { "white-space": "nowrap" },
+    },
+    {
+      dataField: "way",
+      text: "דרך הגעה",
+      type: "string",
+      sort: true,
+      hidden: true,
+      style: { "white-space": "nowrap" },
+    },
+    {
+      dataField: "inv",
+      text: "חשבונית",
+      type: "number",
+      sort: true,
+      hidden: true,
+      style: { "white-space": "nowrap" },
     },
   ]);
   useEffect(() => {
-    fetch("http://localhost:3000/api/getdata")
+    fetch("http://localhost:3000/api/getdata", {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
       .then((res) => res.json())
       .then(
         (result) => {
@@ -62,19 +129,43 @@ function ParticipantsTable() {
       );
   }, []);
 
+  const expandRow = {
+    renderer: (row) => <Expanded row={row} cols={cols} />,
+  };
+
   return (
     <div className="tab">
-      <BootstrapTable
+      <ToolkitProvider
         keyField="id"
         data={data}
         columns={cols}
-        bootstrap4={true}
-        headerClasses="headerRow"
-        rowClasses="rows"
-        striped
-        bordered
-        hover
-      />
+        search={{ searchFormatted: true }}
+      >
+        {(props) => (
+          <div>
+            <SearchBar
+              {...props.searchProps}
+              placeholder="חיפוש"
+              className="searching"
+            />
+            <hr />
+            <BootstrapTable
+              {...props.baseProps}
+              keyField="id"
+              data={data}
+              columns={cols}
+              bootstrap4={true}
+              headerClasses="headerRow"
+              rowClasses="rows"
+              pagination={paginationFactory()}
+              expandRow={expandRow}
+              striped
+              bordered
+              hover
+            />
+          </div>
+        )}
+      </ToolkitProvider>
     </div>
   );
 }
